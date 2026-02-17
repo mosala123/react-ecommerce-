b9c5216da0199c7d47e8edbc43d22c25f4a130da
library <elmosalah74@gmail.com>
2026-02-17 11:08:35 +0200

    upload

--- src/index.js
+++ src/index.js
@@ -6,13 +6,10 @@ import { store } from './component/rtk/store';
 import './index.css';
 import App from './App';
 
-// تحديد basename بناءً على البيئة
-const basename = process.env.NODE_ENV === 'production' ? '/ecommerce-finnally' : '/';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
